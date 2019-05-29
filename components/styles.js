4aaf2a68e4610602eeaa1fcf088ca118225e6696
Eric Fernandes <ericf9333@gmail.com>
2019-05-29 16:05:44 -0100

    segundo

---
+++ components/styles.js
@@ -0,0 +1,33 @@
+import {  StyleSheet } from 'react-native';
+
+
+export const styles = StyleSheet.create({
+  container: {    
+      paddingTop: 30,    
+      flex: 1  
+    },
+  paragraphText: {    
+      fontSize: 16,    
+      lineHeight: 20,  
+    },
+  titleText: {    
+      fontSize: 24,    
+      lineHeight: 30,
+    },
+    primaryButton: {    
+      padding: 20,    
+      backgroundColor: '#124473'  
+    },
+    primaryButtonText: {   
+      color: '#FFF',    
+      textAlign: 'center',  
+    },
+    altButton: {    
+      padding: 20,    
+      backgroundColor: '#23CdA4'  
+    },
+    altButtonText: {    
+        color: '#FFF',    
+        textAlign: 'center',  
+    } 
+}); 
